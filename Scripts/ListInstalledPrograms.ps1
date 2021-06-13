48d684dfec22b45b05270d082602253c86b7815e
Bird <39280243+jlambert360@users.noreply.github.com>
2021-06-13 09:28:08 -0400

    Create ListInstalledPrograms.ps1

---
+++ Scripts/ListInstalledPrograms.ps1
@@ -0,0 +1,2 @@
+# To output to a file edit the path the the end of this command Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > C:\Users\YourName\Documents\InstalledPrograms.txt
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
